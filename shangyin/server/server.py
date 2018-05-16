3459fc0df655c7270991b4911e245f22d8d962fb
Marián Hlaváč <hlavac95@gmail.com>
2018-05-16 23:40:38 +0200

    Finish web stats displaying

--- shangyin/server/server.py
+++ shangyin/server/server.py
@@ -3,18 +3,35 @@ import threading
 import time
 
 dbref = None
+cofcarduser_join = 'JOIN card on card_id=card.id JOIN user ON user_id=user.id'
 
 class ServerRunner(threading.Thread):
     def run(self):
         app = Flask(__name__)
 
         @app.route("/")
-        def hello():
+        def overview():
             global dbref
 
-            data = dbref.select('*', 'coffee')
+            recent = dbref.select('coffee.id, card_id, logged, milk, user_id, fullname, department', 
+                    'coffee', cofcarduser_join + ' ORDER BY logged DESC LIMIT 5'
+                )
 
-            return render_template('index.html', logs=data)
+            counts = dbref.select('department, COUNT(department)', 'coffee',
+                    cofcarduser_join + ' GROUP BY department'
+                )
+
+            return render_template('index.html', recent=recent, counts=counts)
+
+        @app.route("/all")
+        def all():
+            global dbref
+
+            data = dbref.select('coffee.id, card_id, logged, milk, user_id, fullname, department', 
+                    'coffee', cofcarduser_join + ' ORDER BY logged DESC'
+                )
+
+            return render_template('all.html', logs=data)
 
         app.run(host='0.0.0.0', port=8000)
 
